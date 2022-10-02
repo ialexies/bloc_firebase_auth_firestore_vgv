afb114874f299e5e5e6976d251e5e39ff2d58109
alex <ialexies@gmail.com>
2022-10-02 19:17:23 +0800

    convert customError model to freezed

--- lib/models/custom_error.dart
+++ lib/models/custom_error.dart
@@ -1,17 +1,11 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:equatable/equatable.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'custom_error.freezed.dart';
 
-class CustomError extends Equatable {
-  final String code;
-  final String message;
-  final String plugin;
-
-  const CustomError({this.code = '', this.message = '', this.plugin = ''});
-
-  @override
-  List<Object> get props => [code, message, plugin];
-
-  @override
-  String toString() =>
-      'CustomError(code: $code, message: $message, plugin: $plugin)';
+@freezed
+class CustomError with _$CustomError {
+  const factory CustomError({
+    @Default('') String code,
+    @Default('') String message,
+    @Default('') String plugin,
+  }) = _CustomError;
 }
