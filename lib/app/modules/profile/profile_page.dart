bdaac9067a803e8a1c788c5f663ea92f8e9a6419
alex <ialexies@gmail.com>
2022-10-24 23:16:34 +0800

    Fix design of profile

--- lib/app/modules/profile/profile_page.dart
+++ lib/app/modules/profile/profile_page.dart
@@ -1,14 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:developer';
 
-import 'package:bloc_firebase_auth_firestore_vgv/app/modules/auth/auth_bloc.dart';
-import 'package:bloc_firebase_auth_firestore_vgv/app/modules/profile/bloc/profile_cubit.dart';
 import 'package:extended_image/extended_image.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
+import 'package:intl/intl.dart';
+import 'package:bloc_firebase_auth_firestore_vgv/app/modules/auth/auth_bloc.dart';
+import 'package:bloc_firebase_auth_firestore_vgv/app/modules/profile/bloc/profile_cubit.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -83,134 +84,36 @@ class ProfilePageState extends State<ProfilePage> {
               ),
             );
           }
-          return Card(
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
             child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // FadeInImage.assetNetwork(
-                //   placeholder: 'assets/images/loading.gif',
-                //   image: state.user.profileImage,
-                //   width: double.infinity,
-                //   fit: BoxFit.cover,
-                // ),
+                const SizedBox(
+                  height: 30,
+                ),
                 Center(
-                  child: SizedBox(
-                    width: 600.w,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(300),
-                      child: Stack(
-                        children: [
-                          ExtendedImage.network(
-                            state.user.profileImage,
-                            cache: false,
-                            loadStateChanged: (ExtendedImageState state) {
-                              switch (state.extendedImageLoadState) {
-                                case LoadState.loading:
-                                  return const LinearProgressIndicator();
-
-                                case LoadState.completed:
-                                  return ExtendedRawImage(
-                                    image: state.extendedImageInfo?.image,
-                                  );
-
-                                case LoadState.failed:
-                                  return GestureDetector(
-                                    child: Stack(
-                                      fit: StackFit.expand,
-                                      children: const <Widget>[
-                                        Text('No Image'),
-                                        Positioned(
-                                          bottom: 0,
-                                          left: 0,
-                                          right: 0,
-                                          child: Text(
-                                            'load image failed, click to reload',
-                                            textAlign: TextAlign.center,
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                    onTap: () {
-                                      state.reLoadImage();
-                                    },
-                                  );
-                              }
-                            },
-                          ),
-                          Positioned(
-                            bottom: 0,
-                            child: Container(
-                              color: Colors.black.withOpacity(.5),
-                              width: 600.w,
-                              height: 150.w,
-                              child: TextButton(
-                                onPressed: () async {
-                                  XFile? imgFile = XFile('');
-                                  try {
-                                    imgFile = await ImagePicker().pickImage(
-                                      source: ImageSource.camera,
-                                      preferredCameraDevice: CameraDevice.front,
-                                    );
-                                  } catch (e) {}
-
-                                  if (imgFile == null) return;
-
-                                  // context.read<AuthBloc>().add(
-                                  //       UpdatePhotoUrlEvent(
-                                  //         photoUrl: imgFile.path,
-                                  //       ),
-                                  //     );
-
-                                  // await context
-                                  //     .read<ProfileCubit>()
-                                  //     .updateProfilePhoto(await imgFile);
-                                },
-                                child: const Text(
-                                  'Edit',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
+                  child: PhotoBuilder(state),
                 ),
                 const SizedBox(height: 10),
                 Padding(
                   padding: const EdgeInsets.only(left: 15),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        '-id: ${state.user.id}',
-                        style: const TextStyle(fontSize: 18),
+                        toBeginningOfSentenceCase(state.user.name).toString(),
+                        style: TextStyle(
+                          fontSize: 140.sp,
+                        ),
                       ),
-                      const SizedBox(height: 10),
                       Text(
-                        '- name: ${state.user.name}',
-                        style: const TextStyle(fontSize: 18),
+                        state.user.email,
+                        style: TextStyle(
+                          fontSize: 32.sp,
+                          color: Colors.black54,
+                        ),
                       ),
-                      const SizedBox(height: 10),
-                      Text(
-                        '- email: ${state.user.email}',
-                        style: const TextStyle(fontSize: 18),
-                      ),
-                      const SizedBox(height: 10),
-                      Text(
-                        '- point: ${state.user.point}',
-                        style: const TextStyle(fontSize: 18),
-                      ),
-                      const SizedBox(height: 10),
-                      Text(
-                        '- rank: ${state.user.rank}',
-                        style: const TextStyle(fontSize: 18),
-                      ),
+                      const SizedBox(height: 30),
+                      UserDataBuilder(state),
                     ],
                   ),
                 )
@@ -218,6 +121,153 @@ class ProfilePageState extends State<ProfilePage> {
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class UserDataBuilder extends StatelessWidget {
+  const UserDataBuilder(this.state, {super.key});
+  final ProfileState state;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        UserDataRowBuilder(
+          state,
+          Icons.star,
+          toBeginningOfSentenceCase(state.user.rank).toString(),
+        ),
+        const Divider(),
+        UserDataRowBuilder(
+          state,
+          Icons.mail,
+          toBeginningOfSentenceCase(state.user.email).toString(),
+        ),
+        const Divider(),
+        UserDataRowBuilder(
+          state,
+          Icons.add_chart_sharp,
+          toBeginningOfSentenceCase(state.user.point.toString()).toString(),
+        ),
+        UserDataRowBuilder(
+          state,
+          Icons.person,
+          toBeginningOfSentenceCase(state.user.id).toString(),
+        ),
+      ],
+    );
+  }
+}
+
+class UserDataRowBuilder extends StatelessWidget {
+  const UserDataRowBuilder(this.state, this._name, this._val, {super.key});
+  final ProfileState state;
+  final IconData _name;
+  final String _val;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(_name),
+        const SizedBox(width: 10),
+        Text(_val),
+      ],
+    );
+  }
+}
+
+class PhotoBuilder extends StatelessWidget {
+  const PhotoBuilder(this.state, {super.key});
+
+  final ProfileState state;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 600.w,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(300),
+        child: Stack(
+          children: [
+            ExtendedImage.network(
+              state.user.profileImage,
+              cache: false,
+              loadStateChanged: (ExtendedImageState state) {
+                switch (state.extendedImageLoadState) {
+                  case LoadState.loading:
+                    return const LinearProgressIndicator();
+
+                  case LoadState.completed:
+                    return ExtendedRawImage(
+                      image: state.extendedImageInfo?.image,
+                    );
+
+                  case LoadState.failed:
+                    return GestureDetector(
+                      child: Stack(
+                        fit: StackFit.expand,
+                        children: const <Widget>[
+                          Text('No Image'),
+                          Positioned(
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            child: Text(
+                              'load image failed, click to reload',
+                              textAlign: TextAlign.center,
+                            ),
+                          )
+                        ],
+                      ),
+                      onTap: () {
+                        state.reLoadImage();
+                      },
+                    );
+                }
+              },
+            ),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                color: Colors.black.withOpacity(.5),
+                width: 600.w,
+                height: 150.w,
+                child: TextButton(
+                  onPressed: () async {
+                    XFile? imgFile = XFile('');
+                    try {
+                      imgFile = await ImagePicker().pickImage(
+                        source: ImageSource.camera,
+                        preferredCameraDevice: CameraDevice.front,
+                      );
+                    } catch (e) {}
+
+                    if (imgFile == null) return;
+
+                    // context.read<AuthBloc>().add(
+                    //       UpdatePhotoUrlEvent(
+                    //         photoUrl: imgFile.path,
+                    //       ),
+                    //     );
+
+                    // await context
+                    //     .read<ProfileCubit>()
+                    //     .updateProfilePhoto(await imgFile);
+                  },
+                  child: const Text(
+                    'Edit',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
